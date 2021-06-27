2dbde29958b28e35e538a97bb1962be15a381e30
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-06-27 22:07:48 +0300

    header 1.0

--- src/App.js
+++ src/App.js
@@ -5,36 +5,42 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/pages/register';
 import Login from './components/pages/login';
 import Project from './components/pages/project';
+import Header from './components/Header';
+import s from './container.module.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/register">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className={s.container}>
+        <Header />
 
-        <Switch>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/projects">
-            <Project />
-          </Route>
-        </Switch>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/register">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">About</Link>
+              </li>
+              <li>
+                <Link to="/projects">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/projects">
+              <Project />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
