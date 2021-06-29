5cce775f08ffc0ff0aa5a8ff83e3fe81f9e13c66
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-06-29 03:38:27 +0300

    header 1.0

--- src/App.js
+++ src/App.js
@@ -6,41 +6,38 @@ import Register from './components/pages/register';
 import Login from './components/pages/login';
 import Project from './components/pages/project';
 import Header from './components/Header';
-import s from './container.module.css';
 
 function App() {
   return (
     <Router>
-      <div className={s.container}>
-        <Header />
+      <Header />
 
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/register">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">About</Link>
-              </li>
-              <li>
-                <Link to="/projects">Users</Link>
-              </li>
-            </ul>
-          </nav>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/register">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-          <Switch>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/projects">
-              <Project />
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/projects">
+            <Project />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,30 @@
 import { Link } from 'react-router-dom';
 
+// import { ReactComponent as LogOut } from '../../img/LogOut1.svg';
 import logo from '../../img/Logo.svg';
-import s from './Header.module.css';
+import s from './Header.module.scss';
 
 const Header = () => {
+  let isAuthenticated = true;
+
   return (
     <div className={s.header}>
       <Link to="/">
         <img src={logo} className={s.logo} alt="logo GoIT" />
       </Link>
-
-      <div className={s.userMenu}>
-        <>
+      {isAuthenticated && (
+        <div className={s.userMenu}>
           <span className={s.userName}>Username</span>
           <button
             type="button"
             className={s.logOutBtn}
             onClick={() => console.log('Click')}
           >
-            Log out
+            {/* <LogOut fill={'red'} /> */}
+            <span className={s.txtBtn}>Log out</span>
           </button>
-        </>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
