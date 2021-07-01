f1e978360abdc0e919043c14ebbc521c8139b104
LiudmylaKostova <kostolu@ukr.net>
2021-07-01 19:24:53 +0300

    some fixes done

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -6,8 +6,8 @@ import * as yup from 'yup';
 import styles from './login.module.scss';
 
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { authOperations } from '../../redux/auth/';
+import { useDispatch, useSelector } from 'react-redux';
+import { authOperations, authSelectors } from '../../redux/auth/';
 
 const schema = yup.object({
   email: yup
@@ -20,10 +20,19 @@ const schema = yup.object({
     .required('Wrong password'),
 });
 
+const createErrorMessage = error => {
+  if (error.includes('401')) return 'Not valid login or password';
+  if (error.includes('400')) return 'Bad request';
+  if (error.includes('409')) return 'Provided email already exists';
+  return 'Unknown error. Please try again';
+};
+
 const Login = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // const [notification, setNotification] = useState(false);
+  // const [msg, setMsg] = useState('');
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -42,6 +51,14 @@ const Login = () => {
     setSubmitting(false);
     resetForm();
   };
+
+  const errorFromState = useSelector(state =>
+    authSelectors.getErrorMessage(state),
+  );
+
+  const errorMessage = errorFromState
+    ? createErrorMessage(errorFromState)
+    : null;
 
   return (
     <div className={styles.wrapper}>
@@ -81,7 +98,6 @@ const Login = () => {
                 type="password"
                 name="password"
                 placeholder=" "
-                // value="password"
                 className={`${styles.input} ${
                   touched.password && errors.password && styles.errorInput
                 }`}
@@ -91,7 +107,11 @@ const Login = () => {
                 <div className={styles.error}>{errors.password}</div>
               )}
             </label>
-
+            <div className={styles.wr}>
+              {errorMessage ? (
+                <div className={styles.errorMessage}>{errorMessage}</div>
+              ) : null}
+            </div>
             <button className={styles.btn} type="submit">
               Enter
             </button>
@@ -109,87 +129,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Link } from 'react-router-dom';
-// import { Formik, Field, Form } from 'formik';
-// import * as yup from 'yup';
-// import Container from '../Container/Container';
-
-// import styles from './login.module.scss';
-
-// const schema = yup.object({
-//   email: yup
-//     .string('Enter your email')
-//     .email('Enter a valid email')
-//     .required('Email is required'),
-//   password: yup
-//     .string('Enter your password')
-//     .min(8, 'Password should be of minimum 8 characters length')
-//     .required('Wrong password'),
-// });
-
-// const Login = () => (
-//   <div className={styles.wrapper}>
-//     <Container>
-//       <Formik
-//         initialValues={{
-//           email: '',
-//           password: '',
-//         }}
-//         validationSchema={schema}
-//         onSubmit={async values => {
-//           await new Promise(r => setTimeout(r, 500));
-//           alert(JSON.stringify(values, null, 2));
-//         }}
-//       >
-//         {({ errors, touched }) => (
-//           <Form className={styles.form} autoComplete="off">
-//             <h1 className={styles.title}>Enter</h1>
-
-//             <label className={styles.label}>
-//               <Field
-//                 type="text"
-//                 name="email"
-//                 placeholder=" "
-//                 className={`${styles.input} ${
-//                   touched.email && errors.email && styles.errorInput
-//                 }`}
-//               />
-//               <p className={styles.name}>Email</p>
-//               {touched.email && errors.email && (
-//                 <div className={styles.error}>{errors.email}</div>
-//               )}
-//             </label>
-
-//             <label className={styles.label}>
-//               <Field
-//                 type="password"
-//                 name="password"
-//                 placeholder=" "
-//                 className={`${styles.input} ${
-//                   touched.password && errors.password && styles.errorInput
-//                 }`}
-//               />
-//               <p className={styles.name}>Password </p>
-//               {touched.password && errors.password && (
-//                 <div className={styles.error}>{errors.password}</div>
-//               )}
-//             </label>
-
-//             <button className={styles.btn} type="submit">
-//               Enter
-//             </button>
-//             <p className={styles.descr}> No account? </p>
-//             <Link to="/register" className={styles.descr}>
-//               Register
-//             </Link>
-//           </Form>
-//         )}
-//       </Formik>
-//     </Container>
-//   </div>
-// );
-
-// export default Login;
