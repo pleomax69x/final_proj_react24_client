c58c2e9ee42ca8b31d20a740b6a392ad0023f91f
Inga Grigorieva <ingagrigorieva0402@gmail.com>
2021-07-02 16:37:37 +0300

    sidebar done

---
+++ src/components/pages/project.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation, useHistory } from 'react-router';
+import sprintsSelectors from '../../redux/sprints/sprints-selectors';
+import sprintsOperations from '../../redux/sprints/sprints-operations';
+import СreatingSprint from '../СreatingSprint';
+import SprintCard from '../SprintCard';
+import Modal from '../Modal';
+
+const Sprint = () => {
+  const sprints = useSelector(sprintsSelectors.getSprints);
+
+  const dispatch = useDispatch();
+
+  const deleteSprint = id => dispatch(sprintsOperations.deleteSprint(id));
+
+  useEffect(() => {
+    dispatch(sprintsOperations.getSprints());
+  }, [dispatch]);
+
+  const location = useLocation();
+
+  const history = useHistory();
+  const addSprints = id => history.push(`/sprints/${id}`, id);
+
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = useCallback(() => {
+    setShowModal(prevShowModal => !prevShowModal);
+  }, []);
+
+  return (
+    <div>
+      <h1>Sprints</h1>
+      <button type="button" onClick={toggleModal}>
+        Сreat sprint
+      </button>
+      <ul>
+        {sprints.map(sprints => (
+          <SprintCard
+            key={sprints.id}
+            title={sprints.title}
+            date={sprints.date}
+            duration={sprints.duration}
+            to={() => addSprints(sprints.id)}
+            onClick={() => deleteSprint(sprints.id)}
+          />
+        ))}
+      </ul>
+
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <СreatingSprint onSave={toggleModal} prId={location.state} />
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default Sprint;
+
+// ============ СreatingProject
+// import React, { useState, useCallback } from 'react';
+// import СreatingProject from '../СreatingProject';
+// import Modal from '../Modal';
+
+// const Project = () => {
+//   const [showModal, setShowModal] = useState(false);
+
+//   const toggleModal = useCallback(() => {
+//     setShowModal(prevShowModal => !prevShowModal);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Project</h1>
+//       <button type="button" onClick={toggleModal}>
+//         Сreat project
+//       </button>
+
+//       {showModal && (
+//         <Modal onClose={toggleModal}>
+//           <СreatingProject onSave={toggleModal} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Project;
