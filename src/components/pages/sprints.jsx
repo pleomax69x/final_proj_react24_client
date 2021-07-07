dcc0b0459982c3eee182048837d0d0459cc3e158
Andrey <56323140+AndreyNazarov@users.noreply.github.com>
2021-07-07 22:02:47 +0300

    add teammates

--- src/components/pages/sprints.jsx
+++ src/components/pages/sprints.jsx
@@ -2,10 +2,14 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 import sprintsSelectors from '../../redux/sprints/sprints-selectors';
+import peopleSelectors from '../../redux/peopleAdd/people-selectors';
 import sprintsOperations from '../../redux/sprints/sprints-operations';
 import СreatingSprint from '../СreatingSprint/СreatingSprint.js';
+import СreatingPeopleItem from '../AddPeopleItem/CreatingPeopleItem';
 import SprintsItem from '../SprintsItem';
-import Modal from '../Modal';
+import AddPeople from '../AddPeopleItem/PeopleItem';
+import Modal from '../Modal/Modal';
+import PeopleModal from '../Modal/PeopleModal';
 
 const Sprint = () => {
   const dispatch = useDispatch();
@@ -18,12 +22,18 @@ const Sprint = () => {
   const token = JSON.parse(getStorageData).token;
 
   const sprints = useSelector(sprintsSelectors.getSprints);
+  const teammate = useSelector(peopleSelectors.getPeople);
 
   const projectId = history.location.state;
   const addSprints = id => history.push(`/projects/${projectId}/${id}`, id);
   const [showModal, setShowModal] = useState(false);
+  const [addPeopleModal, setAddPeopleModal] = useState(false);
   const toggleModal = useCallback(() => {
     setShowModal(prevShowModal => !prevShowModal);
+  }, []);
+
+  const togglePeopleModal = useCallback(() => {
+    setAddPeopleModal(prevShowModal => !prevShowModal);
   }, []);
 
   useEffect(() => {
@@ -49,6 +59,17 @@ const Sprint = () => {
         <Modal onClose={toggleModal}>
           <СreatingSprint onSave={toggleModal} prId={projectId} />
         </Modal>
+      )}{' '}
+      <AddPeople
+        teammate={teammate}
+        // to={addSprints}
+        // del={deleteSprint}
+        toggleModal={togglePeopleModal}
+      />
+      {addPeopleModal && (
+        <PeopleModal onClose={togglePeopleModal}>
+          <СreatingPeopleItem onSave={toggleModal} prId={projectId} />
+        </PeopleModal>
       )}
     </div>
   );
